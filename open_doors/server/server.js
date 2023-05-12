3de19209bac3f01a3e791470f8a71faf982c0b4b
cesar-gz <cesarg7@csu.fullerton.edu>
2023-05-12 08:25:16 -0700

    setting up restaurants db

--- open_doors/server/server.js
+++ open_doors/server/server.js
@@ -1,21 +1,30 @@
 const express = require("express")
 const axios = require("axios")
 //import fetch from 'node-fetch';
+
+import cors from "cors"
+//import restaurants from "./api/restaurants.route.js"
+
 const app = express()
 const PORT = 5000
 
 
+app.use(cors())
+app.use(express.json)
+//app.use("/api/v1/restaurants", restaurants)
 
-app.get("/api", (req, res) => 
+app.get("/api", (req, res) =>
     axios
       .get("http://localhost:3001/businesses")
       .then(response => res.json(response.data))
 )
 
 app.get("/admin", (req, res) => {
-  
+
 })
 
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`)
 })
+
+//export default app
